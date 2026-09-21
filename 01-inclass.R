d5963eb1feda0e0ef52340d73123aaaa62537570
pjmeyer-hub <patriciameyer019@gmail.com>
2026-09-21 13:19:25 -0400

    In Cass Assignment

--- 01-inclass.R
+++ 01-inclass.R
@@ -27,11 +27,19 @@ library(tidyverse)
 # Modify the code below such that the grade shows in increasing order
 # and all category labels start with a capital letter
 
+
 install.packages("flextable")
 library(flextable)
 
+install.packages("dplyr")
+library(dplyr)
+
 yrbss$Grade <- yrbss$grade
-yrbss$Gender <- yrbss$gender
+yrbss$Gender <- yrbss$gender 
+
+yrbss <- yrbss %>% mutate(Grade = fct_relevel(Grade, c("9", "10", "11", "12", "other")))
+yrbss <- yrbss %>% mutate(Gender = str_to_title(Gender)) %>%
+  mutate(Grade = str_to_title(Grade))
 
 z <- summarizor(
   yrbss[c("Grade", "Gender")],
@@ -57,10 +65,38 @@ aggregate(xxx) |>
 ...
 
 
+aggregate(physically_active_7d ~ Grade + Gender, data = yrbss, FUN = mean, na.rm=TRUE) %>%
+  ggplot(yrbss, 
+  mapping = aes(x = Grade,
+  y = physically_active_7d,
+color = Gender,
+group = Gender)) +
+  geom_point() +
+  geom_line() +
+  labs(x = "Grade",
+y = "Number of Days Physically Active",
+title = "Number of Days Physically Active by Grade") +
+  scale_y_continuous(limits = c(0, 7))
+
+
 # Create a plot that shows the relationship betwen physical activity and bmi
 # among female students in grade 12 
 # Ensure that the figure is clearly labeled and includes an appropriate legend
 
+yrbss$bmi <- yrbss$weight/(yrbss$height^2)
+library(ggplot2)
+data(yrbss)
 
+yrbss %>% 
+  dplyr::filter(Grade == 12, Gender == "Female") %>%
+  ggplot( 
+  mapping = aes(x = physically_active_7d,
+  y = bmi)) +
+  geom_point() +
+  geom_smooth(method = 'lm', formula = y~x)
+  labs(x = "Days of Physical Activity",
+y = "BMI",
+title = "Physical Activity and BMI Among Grade 12 Girls") +
+  scale_y_continuous(limits = c(0, NA))
 
 # Push your completed code to your GitHub repository
